1d7e68b2bfc0aeecccbfcddeebd66a0d4df8c0b5
korivernon <44047473+korivernon@users.noreply.github.com>
2019-07-08 10:50:01 -0400

    latest

--- Spreadsheet Viewer/gsheets_imp.py
+++ Spreadsheet Viewer/gsheets_imp.py
@@ -21,18 +21,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 data_json = basedir+'/client_secret.json'
 credentials = ServiceAccountCredentials.from_json_keyfile_name(data_json, scope)
 client = gspread.authorize(credentials)
-# Specify the file and sheet that you want to use. 
+# Specify the file and sheet that you want to use.
 sheet = client.open("Triangle Black Businesses (Responses)").sheet1
-# Get the records on the sheet that is passed in. 
+# Get the records on the sheet that is passed in.
 list_of_hashes = sheet.get_all_records()
 # Print the Keys in the dictionary to offer them as choices.
 # Initialize the count to 0 so that we can output them as choices.
-# Better interface for testing in Python IDLE. 
+# Better interface for testing in Python IDLE.
 options = [key for key in list_of_hashes[0].keys()]
 
 # We don't want to have all of the options available to be chosen.
 # You want to limit the amount of information available to the user to see.
-# The user does not need to see everything. 
+# The user does not need to see everything.
 limit_option = options[2:6:]
 
 # Print the Outputs
@@ -48,19 +48,21 @@ print("==========================")
     # Output for the options.
 
 search = input("Database Query (Select Number): ")
+result = limit_option[int(search)-1]
 # search = 'Products or Services Offered'
 
 print(limit_option[1])
 print(search,"Results")
 db_elements_num = len(list_of_hashes)
+
 for _ in range(db_elements_num):
-    #fix if statements 
-    if (limit_option[int(search)-1] in options):
+    #fix if statements
+    if (result in options):
         # Printing the Businesses
-        print((_+1),list_of_hashes[_][limit_option[int(search)-1]])
-    if (limit_option[int(search)-1] in options):
-        # Searching the Product or Service Offered. 
-        print(list_of_hashes[_]['Business Name']+":\n  -",list_of_hashes[_][limit_option[int(search)-1]])
+        print((_+1),list_of_hashes[_][result])
+    if (result in options):
+        # Searching the Product or Service Offered.
+        print(list_of_hashes[_]['Business Name']+":\n  -",list_of_hashes[_][result])
     if (int(search)-1 == limit_option[1]):
         # Printing the Business Contact Information
         print(list_of_hashes[_]['Business Name']+":\n  -",list_of_hashes[_]['Products or Services Offered'],list_of_hashes[_]['Business Contact (Owner)'],list_of_hashes[_]['Business Number'])
